bb2c7508e9aa7a98146b1bee0ee8f6214192b2ad
PaulC56 <119007257+PaulC56@users.noreply.github.com>
2025-01-21 00:06:12 -0500

    added favorite number input

--- helloworld1.cpp
+++ helloworld1.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int main() {
     cout << "Hello World!";
+    cout << "Here are some additional modifications" << endl;
+    cout << "Whats your favorit number?\n";
+    int fav;
+    cin >> fav;
+    cout << fav;
+    
+    return 0;
 }
